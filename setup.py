d6bffe45b2feb6c5492e3a6c7c4fe00b64b8d66c
Steve Lamb <slamb@azavea.com>
2013-09-20 14:39:29 -0400

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='Django Queryset CSV Writer',
+    version='0.1.1',
+    author='Steve Lamb',
+    author_email='slamb@azavea.com',
+    packages=['djqscsv'],
+    requires=['django'],
+    url='http://github.com/steventlamb/django-queryset-csv/',
+    description='A simple python module for writing querysets to csv',
+    keywords="django queryset csv",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+    ]
+)
