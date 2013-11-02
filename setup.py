b502681e9bbea1aa37e5b4f1945b2b6e5a7dd69b
Steve Lamb <slamb@azavea.com>
2013-11-01 21:06:48 -0400

    Add support for uploading to PyPI

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-from distutils.core import setup
-import setuptools  # NOQA
+from setuptools import setup, find_packages
+
 
 setup(
-    name='Django Queryset CSV Writer',
+    name='django-queryset-csv',
     version='0.1.1',
+    description='A simple python module for writing querysets to csv',
     author='Steve Lamb',
     author_email='slamb@azavea.com',
-    packages=['djqscsv'],
-    requires=['django'],
-    url='http://github.com/steventlamb/django-queryset-csv/',
-    description='A simple python module for writing querysets to csv',
+    url='http://github.com/azavea/django-queryset-csv',
+    packages=find_packages(exclude=('tests',)),
     keywords="django queryset csv",
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-    ]
+        "Environment :: Plugins",
+        "Framework :: Django",
+        "License :: OSI Approved :: GNU General Public License (GPL)"
+    ],
+    install_requires=['django>=1.5'],
 )
