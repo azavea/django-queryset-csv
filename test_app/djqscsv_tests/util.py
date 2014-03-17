4abdfd5741ddc033a47a5cb7de21623ffb41bfd7
Steve Lamb <slamb@azavea.com>
2014-03-17 13:31:54 -0400

    Only create seed data once

--- test_app/djqscsv_tests/util.py
+++ test_app/djqscsv_tests/util.py
@@ -1,15 +1,15 @@
 from .models import Person, Activity
 
 def create_people_and_get_queryset():
-    doing_magic = Activity.objects.create(name="Doing Magic")
-    resting = Activity.objects.create(name="Resting")
+    doing_magic, _ = Activity.objects.get_or_create(name="Doing Magic")
+    resting, _ = Activity.objects.get_or_create(name="Resting")
 
-    Person.objects.create(name='vetch', address='iffish',
-                          info='wizard', hobby=doing_magic)
-    Person.objects.create(name='nemmerle', address='roke',
-                          info='deceased arch mage', hobby=resting)
-    Person.objects.create(name='ged', address='gont',
-                              info='former arch mage', hobby=resting)
+    Person.objects.get_or_create(name='vetch', address='iffish',
+                                 info='wizard', hobby=doing_magic)
+    Person.objects.get_or_create(name='nemmerle', address='roke',
+                                 info='deceased arch mage', hobby=resting)
+    Person.objects.get_or_create(name='ged', address='gont',
+                                 info='former arch mage', hobby=resting)
 
     return Person.objects.all()
 
