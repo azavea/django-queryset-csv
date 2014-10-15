9ba2e859e06090c351a5de39a8cc1db62ac8b3a1
Steve Lamb <slamb@azavea.com>
2014-10-15 11:39:57 -0400

    Add unit test for aggregation

--- djqscsv/__init__.py
+++ djqscsv/__init__.py
@@ -1,2 +1,2 @@
 from djqscsv import (render_to_csv_response, write_csv,  # NOQA
-                             generate_filename, CSVException)  # NOQA
+                     generate_filename, CSVException)  # NOQA
